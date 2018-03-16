084f0dacfd9391ead8d557f90a18b08fe07b5090
maxastuart <maxastuart@gmail.com>
2018-03-16 00:21:42 +0000

    shell.c outline and header

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <...>
+
+void type_prompt(void);
+
+void read_cmd(char *cmd, char **param);
+
+#endif __HEADER__
---
+++ shell.c
@@ -0,0 +1,29 @@
+#include "header.h"
+
+/**
+ * type_print - displays a prompt asking the user to input a command
+ */
+void type_print(void)
+{
+	
+}
+
+/**
+ * read_cmd - reads user input for command to execute, along with parameters
+ * @cmd: text string of command name
+ * @param: array of text strings of parameters
+ */
+void read_cmd(char *cmd, char **param)
+{
+	
+}
+
+/**
+ * main - looping shell function, executing command if correctly entered
+ *
+ * Return: -1 on error, otherwise nothing (gives control to executed command)
+ */
+int main(void)
+{
+	
+}
