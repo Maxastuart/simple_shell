533e240dd5744a00d98e98d14610c4b56601d58e
maxastuart <maxastuart@gmail.com>
2018-03-26 01:20:34 +0000

    cleaned up header

--- header.h
+++ header.h
@@ -14,34 +14,25 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+
 #define delim "\n "
 
 extern char **environ;
 
 void type_prompt(void);
-
 void read_cmd(char **param);
-
 int location_check(char *cmd);
-
 char *find_cmd(char *av, int count, char *param);
-
 void _kill(char *lineptr);
-
 void env(void);
-
 int write_nope(char *av, int count, char *param);
 
 char *_itoa(int n);
 
 int _strlen(char *s);
-
 int _strcmp(char *s1, char *s2);
-
 int _strncmp(char *s1, char *s2, int n);
-
 char *_strcat(char *dest, char *src);
-
 char *_strcpy(char *dest, char *src);
 
 
