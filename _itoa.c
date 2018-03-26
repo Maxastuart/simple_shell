31a0adfe7757a50280a27510cefe021c98d15806
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-26 01:11:27 +0000

    convert int to string

---
+++ _itoa.c
@@ -0,0 +1,32 @@
+#include "header.h"
+
+/**
+ *
+ */
+char *_itoa(int n)
+{
+	int copy, j, i = 0;
+	char *str;
+
+	copy = n;
+
+	while (n != 0)
+	{
+		i++;
+		n = n/10;
+	}
+
+	str = malloc(i * sizeof(char) + 1);
+	if (str == NULL)
+		return(NULL);
+	str[i] = '\0';
+	i--;
+
+	while (i >= 0)
+	{
+		str[i] = copy % 10 + '0';
+		copy = copy / 10;
+		i--;
+	}
+	return(str);
+}
