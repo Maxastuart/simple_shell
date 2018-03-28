344088b995ccb6fa07bd5bfe9c2e35ffa0b1de85
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-28 00:51:49 +0000

    Function that prints message if fork fails

---
+++ fork_fail.c
@@ -0,0 +1,14 @@
+#include "header.h"
+
+/**
+ * fork_fail - prints fork error message if fork fails
+ *@pid: value of pid
+ */
+
+void fork_fail(int pid_value)
+{
+	write(STDOUT_FILENO, "fork failed with error code= ", 29);
+	write(STDOUT_FILENO, _itoa(pid_value), _strlen(_itoa(pid_value)));
+	write(STDOUT_FILENO, "\n", 2);
+	write(STDERR_FILENO, "FORK error\n", 12);
+}
