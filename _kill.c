6d509577a08d188dea45c1bd35128a64f4bf0c8f
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-28 02:15:49 +0000

    added tmp and tok parameter input to be freed

--- _kill.c
+++ _kill.c
@@ -5,8 +5,10 @@
  * @lineptr: string of stdin from read_cmd()
  */
 
-void _kill(char *lineptr)
+void _kill(char *lineptr, char *tmp, char **tok)
 {
+	free(tmp);
+	free(tok);
 	free(lineptr);
 	_exit(1);
 }
